ec114f9730f5cd4911a67a8abcc323797ce625f8
ECCC-CSSP <charles.leblanc2@canada.ca>
2019-12-06 17:04:24 -0400

    Init

--- src/play/ConsoleAppHelloWorld/Program.cs
+++ src/play/ConsoleAppHelloWorld/Program.cs
@@ -2,8 +2,15 @@
 
 namespace ConsoleAppHelloWorld
 {
+    /// <summary>
+    /// This is the program class
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// This is the summary of Main
+        /// </summary>
+        /// <param name="args">Array of arguments</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
